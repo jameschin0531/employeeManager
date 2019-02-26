487c40fc810da509fbd2acedfd98e1631df52acb
James Chin <jameschin0531@gmail.com>
2019-02-26 16:20:56 +0800

    initial

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyC-tLuFBqVNmFrQ0F-mtbiNoFTe9_nPxJg",
+  authDomain: "vuefs-dev-7cb24.firebaseapp.com",
+  databaseURL: "https://vuefs-dev-7cb24.firebaseio.com",
+  projectId: "vuefs-dev-7cb24",
+  storageBucket: "vuefs-dev-7cb24.appspot.com",
+  messagingSenderId: "67747802582"
+};
